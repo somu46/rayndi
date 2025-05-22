d0ea66a26d441554c84c7be29f2921f05d8244a4
somu46 <soumyajitray2002@gmail.com>
2025-05-23 04:34:57 +0530

    Update App component to add a link to meet the CEO

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
      <h1>Hello,my name is Soumyajit Ray</h1>
       <h2> I am a Randi Provider</h2>
       <a href='tel:+919123813528'>Call me</a>
-
+      <a href='https://www.facebook.com/snehasish.nag.79'>meet our CEO</a>
     </div>
   );
 }

d219f0c65eae4716631a978ae012c3e90f478ec9
Soumyajit Ray <101827920+somu46@users.noreply.github.com>
2025-06-09 19:15:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Hello,my name is Soumyajit Ray</h1>
-      <h2> I am a Randi Provider</h2>
-      <a href='tel:+919123813528'>Call me</a>
-      <a href='https://www.facebook.com/snehasish.nag.79'>meet our CEO</a>
+     <h1>Hello,my name is Soumyajit Ray. I am the owner of this domain </h1>
+      <p>Contact me 9123813528</p>
     </div>
   );
 }

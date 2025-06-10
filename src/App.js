8356dc1bca007f7234ca22e8bd1435e1172f340e
Soumyajit Ray <soumyajitray2002@gmail.com>
2025-06-11 00:56:04 +0530

    `Remove unused contact information and update App component`

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-     <h1>Hello,my name is Soumyajit Ray. I am the owner of this domain </h1>
-      <p>Contact me 9123813528</p>
-=======
-     
->>>>>>> 2c58bc3 (Remove unused favicon and logo images; update App component to clean up code)
+    <h1>Rayndi</h1>
     </div>
   );
 }

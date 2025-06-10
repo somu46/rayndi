8b05be1e2c5790cb29a24ee259fcaca84bdeeda5
somu46 <soumyajitray2002@gmail.com>
2025-06-11 00:45:24 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
      <h1>Hello,my name is Soumyajit Ray. I am the owner of this domain </h1>
       <p>Contact me 9123813528</p>
+=======
+     
+>>>>>>> 2c58bc3 (Remove unused favicon and logo images; update App component to clean up code)
     </div>
   );
 }

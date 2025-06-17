a0f450be7b470b022c5dec873d463794896c946f
Soumyajit Ray <soumyajitray2002@gmail.com>
2025-06-18 01:21:58 +0530

    `Removed unused import from Home.js`

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { FiArrowRight, FiCheck, FiGlobe, FiUsers, FiAward, FiLayers } from "react-icons/fi";
+import { FiArrowRight, FiGlobe, FiUsers, FiAward, FiLayers } from "react-icons/fi";
 import OurProcess from '../../Components/UIVerse/OurProcess'
 const HomePage = () => {
   // Features data

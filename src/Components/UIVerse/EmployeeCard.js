6632d3f4bb7444ba5c96b4c2ee528d1b11681685
Soumyajit Ray <soumyajitray2002@gmail.com>
2025-06-29 03:27:40 +0530

    `Updated EmployeeCard.js: changed icon import, updated card dimensions, and modified CSS classes`

--- src/Components/UIVerse/EmployeeCard.js
+++ src/Components/UIVerse/EmployeeCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaXTwitter, FaGithub, FaLinkedin } from 'react-icons/fa6';
 
 const Card = ({ member }) => {
   return (
-    <div className="w-[280px] h-[280px] bg-black rounded-[32px] p-[3px] relative shadow-[0px_70px_30px_-50px_rgba(96,75,74,0.19)] transition-all duration-500 ease-in-out group hover:rounded-tl-[55px]">
+    <div className="w-[250px] h-[250px] bg-red-600 rounded-[32px] p-[3px] relative shadow-[0px_70px_30px_-50px_rgba(96,75,74,0.19)] transition-all duration-500 ease-in-out group hover:rounded-tl-[55px]">
       <div className="absolute w-[calc(100%-6px)] h-[calc(100%-6px)] top-[3px] left-[3px] rounded-[29px] z-[1] overflow-hidden transition-all duration-500 ease-in-out delay-200 group-hover:w-[100px] group-hover:h-[100px] group-hover:top-[10px] group-hover:left-[10px] group-hover:rounded-full group-hover:z-[3] group-hover:border-[7px] border-blue-700 group-hover:shadow-[0px_5px_5px_0px_rgba(96,75,74,0.19)]">
         <img
           src={member.image}
@@ -12,21 +12,21 @@ const Card = ({ member }) => {
         />
       </div>
 
-      <div className="absolute bottom-[3px] left-[3px] right-[3px] top-[80%] bg-blue-500 rounded-[29px] z-[2] shadow-inner transition-all duration-[500ms] ease-[cubic-bezier(0.645,0.045,0.355,1)] group-hover:top-[20%] group-hover:rounded-[80px_29px_29px_29px] delay-200 overflow-hidden">
-        <div className="absolute bottom-0 left-6 right-6 h-[160px]">
+      <div className=" absolute bottom-[3px] left-[3px] right-[3px] top-[80%] bg-blue-500 rounded-[29px] z-[2] shadow-inner transition-all duration-[500ms] ease-[cubic-bezier(0.645,0.045,0.355,1)] group-hover:top-[20%] group-hover:rounded-[80px_29px_29px_29px] delay-200 overflow-hidden">
+        <div className="absolute bottom-0 left-6 right-6 h-[140px] ">
           <span className="block text-white font-bold text-[1.2rem]">
             {member.name}
           </span>
-          <span className="block text-white text-[0.9rem] mt-4">
+          <span className="block text-white text-[0.9rem] mt-2">
             {member.role}
           </span>
         </div>
 
-        <div className="absolute bottom-4 left-6 right-6 flex ">
+        <div className="absolute bottom-4 left-6 right-6 flex">
           <div className="flex gap-6 mx-auto">
             {member?.social?.twitter && (
               <a href={member.social.twitter} target="_blank" rel="noopener noreferrer">
-                <FaTwitter className="h-5 text-white drop-shadow-[0_5px_5px_rgba(165,132,130,0.13)] hover:text-[#f55d56] hover:scale-[1.2] transition-transform duration-300" />
+                <FaXTwitter className="h-5 text-white drop-shadow-[0_5px_5px_rgba(165,132,130,0.13)] hover:text-[#f55d56] hover:scale-[1.2] transition-transform duration-300" />
               </a>
             )}
             {member?.social?.linkedin && (
